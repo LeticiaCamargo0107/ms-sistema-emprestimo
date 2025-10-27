686f0bc7ca08e127a653701b28249ed2ffd75e6b
LeticiaCamargo0107 <leticia.camargo@picpay.com>
2025-10-27 16:38:55 -0300

    create controller and get methods
    :qa

--- src/main/java/com/example/InstalllmentSystem/entrypoint/controller/PaymentController.java
+++ src/main/java/com/example/InstalllmentSystem/entrypoint/controller/PaymentController.java
@@ -1,4 +1,18 @@
 package com.example.InstalllmentSystem.entrypoint.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+
+@RestController
+@RequestMapping("/v1/payment")
 public class PaymentController {
+
+    @GetMapping("/amount/{amount}")
+    public BigDecimal getByAmount (BigDecimal amount){
+        return amount;
+    }
 }
