12d2ebb9f6e9eb5f233bcc851dc3e46360eb9af6
LeticiaCamargo0107 <leticia.camargo@picpay.com>
2025-11-11 15:54:58 -0300

    refactor CustomHandler and ApiErros to return List of erros, add dependency in pom.xml

--- src/main/java/com/example/InstalllmentSystem/entrypoint/handler/ApiError.java
+++ src/main/java/com/example/InstalllmentSystem/entrypoint/handler/ApiError.java
@@ -1,17 +1,27 @@
 package com.example.InstalllmentSystem.entrypoint.handler;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
 
     private String message;
     private String code;
+    private List<String> erros;
 
     public ApiError (String message, String code) {
         this.message = message;
+        this.code = code;
+    }
+
+    public ApiError (List<String> erros, String code) {
+        this.erros = erros;
         this.code = code;
     }
 }
