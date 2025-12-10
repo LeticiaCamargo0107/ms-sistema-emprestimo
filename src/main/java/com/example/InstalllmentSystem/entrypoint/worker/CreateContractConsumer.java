c967893d181eb59c445d2120f2226ab7b6089cea
LeticiaCamargo0107 <leticia.camargo@picpay.com>
2025-12-10 10:32:17 -0300

    Put anotation in ConsumerClass

--- src/main/java/com/example/InstalllmentSystem/entrypoint/worker/CreateContractConsumer.java
+++ src/main/java/com/example/InstalllmentSystem/entrypoint/worker/CreateContractConsumer.java
@@ -8,10 +8,12 @@ import com.example.InstalllmentSystem.entrypoint.mapper.ContractMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 
 import java.util.function.Consumer;
 
+@Configuration
 @RequiredArgsConstructor
 @Slf4j
 public class CreateContractConsumer {
